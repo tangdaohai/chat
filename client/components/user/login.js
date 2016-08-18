8b8221a055bbd1596c673e8b51d52734eb0d6676
Jerry Tang <tangdaohai@ptthink.com>
2016-08-18 18:23:42 +0800

    修改react-bootstrap 为 ant-design

--- client/components/user/login.js
+++ client/components/user/login.js
@@ -1,51 +1,46 @@
 /**
- * Created by Jerry on 16/8/10.
+ * Created by Jerry on 16/8/18.
  */
 
 import React from "react";
-import { Form, FormGroup, Col, FormControl, Checkbox, Button, ControlLabel } from "react-bootstrap";
+import { Form, Input, Button, Checkbox } from 'antd';
+const FormItem = Form.Item;
 
-export default class Login extends React.Component{
+class Login extends React.Component{
 
-    render(){
-        return <div style={{marginTop : "150px"}}>
-            <Form horizontal>
-                <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} sm={4}>
-                        Email
-                    </Col>
-                    <Col sm={6}>
-                        <FormControl type="email" placeholder="Email" />
-                    </Col>
-                </FormGroup>
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('收到表单值：', this.props.form.getFieldsValue());
+    }
 
-                <FormGroup controlId="formHorizontalPassword">
-                    <Col componentClass={ControlLabel} sm={4}>
-                        Password
-                    </Col>
-                    <Col sm={6}>
-                        <FormControl type="password" placeholder="Password" />
-                    </Col>
-                </FormGroup>
+    render() {
+        const { getFieldProps } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 }
+        };
 
-                <FormGroup>
-                    <Col smOffset={4} sm={4}>
-                        <Checkbox>Remember me</Checkbox>
-                    </Col>
-                </FormGroup>
+        return <div style={{ margin : "120px auto", maxWidth : "480px" }}>
+            <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                <FormItem {...formItemLayout} label="邮箱 :">
+                    <Input placeholder="请输入邮箱" {...getFieldProps('userName')}/>
+                </FormItem>
 
-                <FormGroup>
-                    <Col smOffset={4} sm={4}>
-                        <Button>
-                            登录
-                        </Button>
-                        {"  "}
-                        <Button bsStyle="link">
-                            注册新账号
-                        </Button>
-                    </Col>
-                </FormGroup>
+                <FormItem {...formItemLayout} label="密码 :">
+                    <Input type="password" {...getFieldProps('pass', { initialValue: '' })} placeholder="请输入密码" />
+                </FormItem>
+
+                <FormItem wrapperCol={{ span: 16, offset: 6 }}>
+                    <Checkbox {...getFieldProps('remember', { initialValue: false, valuePropName: 'checked' })}>记住密码</Checkbox>
+                </FormItem>
+
+                <FormItem wrapperCol={{ span: 16, offset: 6 }}>
+                    <Button type="primary" htmlType="submit">确定</Button>
+                    <a style= {{marginLeft : "20px"}}>注册新账户</a>
+                </FormItem>
             </Form>
-        </div>
+        </div>;
     }
 }
+
+export default Form.create()(Login);
